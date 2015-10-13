befc21976cb114fcbbab01eaf797d74886b225f4
sakuramilk <c.sakuramilk@gmail.com>
2015-10-13 18:17:28 +0900

    Revert "update build system"
    This reverts commit 0f7c063135229e0d50b4f4fd99a9f0bc2c6af3b8.

--- build_msm8974pro_kernel.sh
+++ build_msm8974pro_kernel.sh
@@ -5,7 +5,7 @@ BUILD_TOP_DIR=..
 BUILD_KERNEL_DIR=$(pwd)
 
 SECURE_SCRIPT=$BUILD_TOP_DIR/../buildscript/tools/signclient.jar
-BUILD_CROSS_COMPILE=/opt/toolchains/arm-eabi-4.8/bin/arm-eabi-
+BUILD_CROSS_COMPILE=../prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi-
 BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
 
 # Default Python version is 2.7
@@ -14,8 +14,9 @@ ln -sf /usr/bin/python2.7 ./bin/python
 export PATH=$(pwd)/bin:$PATH
 
 KERNEL_DEFCONFIG=msm8974_sec_defconfig
+DEBUG_DEFCONFIG=msm8974_sec_eng_defconfig
 SELINUX_DEFCONFIG=selinux_defconfig
-#SELINUX_LOG_DEFCONFIG=selinux_log_defconfig
+SELINUX_LOG_DEFCONFIG=selinux_log_defconfig
 
 #sed -i.bak "s/CONFIG_MODVERSIONS=y/CONFIG_MODVERSIONS=n/g" ${BUILD_KERNEL_DIR}/arch/arm/configs/${KERNEL_DEFCONFIG}
 
@@ -39,24 +40,8 @@ done
 shift $((OPTIND-1))
 
 BUILD_COMMAND=$1
-
-SEANDROID_OPTION=
-SECURE_OPTION=
-if [ "$2" == "-B" ]; then
-	SECURE_OPTION=$2
-elif [ "$2" == "-E" ]; then
-	SEANDROID_OPTION=$2
-else
-	NO_JOB=
-fi
-
-if [ "$3" == "-B" ]; then
-	SECURE_OPTION=$3
-elif [ "$3" == "-E" ]; then
-	SEANDROID_OPTION=$3
-else
-	NO_JOB=
-fi
+SECURE_OPTION=$2
+SEANDROID_OPTION=$3
 
 if [ "$BUILD_COMMAND" == "klte_eur" ]; then
 	SIGN_MODEL=SM-G900F_EUR_XX_ROOT0
@@ -70,10 +55,6 @@ elif [ "$BUILD_COMMAND" == "klte_spr" ]; then
 	SIGN_MODEL=SM-G900P_NA_SPR_ROOT0
 elif [ "$BUILD_COMMAND" == "klte_usc" ]; then
 	SIGN_MODEL=SM-G900R4_NA_USC_ROOT0
-elif [ "$BUILD_COMMAND" == "kactivelte_att" ]; then
-	SIGN_MODEL=SM-G870A_NA_ATT_ROOT0
-elif [ "$BUILD_COMMAND" == "ksportslte_spr" ]; then
-	SIGN_MODEL=SM-G860P_NA_SPR_ROOT0
 else
 	SIGN_MODEL=
 fi
@@ -83,28 +64,9 @@ TEMP=${BUILD_COMMAND#*_}
 REGION=${TEMP%%_*}
 CARRIER=${TEMP##*_}
 
-if [[ "$BUILD_COMMAND" == "klte_dcm"* ]]; then		# KLTEDCM
-	VARIANT=k${CARRIER}
-	DTS_NAMES=msm8974pro-ac-sec-kjpn-
-elif [[ "$BUILD_COMMAND" == "klte"* ]]; then		# KLTE
-	VARIANT=k${CARRIER}
-	DTS_NAMES=msm8974pro-ac-sec-k-
-elif [[ "$BUILD_COMMAND" == "kactivelte"* ]]; then	# KACTIVE
-	VARIANT=kactive${CARRIER}
-	DTS_NAMES=msm8974pro-ac-sec-kactivelte-
-elif [[ "$BUILD_COMMAND" == "ksportslte"* ]]; then	# KSPORTS
-	VARIANT=ksports${CARRIER}
-	DTS_NAMES=msm8974pro-ac-sec-ksports-
-elif [[ "$BUILD_COMMAND" == "slte"* ]]; then		# SLTE
-	VARIANT=s${CARRIER}
-	DTS_NAMES=msm8974pro-ac-sec-s-
-elif [[ "$BUILD_COMMAND" == "pateklte"* ]]; then	# PATEKLTE
-	VARIANT=patek${CARRIER}
-	DTS_NAMES=msm8974pro-ac-sec-patek-
-else
-	DTS_NAMES=
-fi
-
+VARIANT=k${CARRIER}
+DTS_NAMES=msm8974pro-ac-sec-k-
+#DTS_NAMES=msm8974pro-ac-sec
 PROJECT_NAME=${VARIANT}
 VARIANT_DEFCONFIG=msm8974pro_sec_${MODEL}_${CARRIER}_defconfig
 
@@ -119,18 +81,17 @@ case $1 in
 		#rm $BUILD_KERNEL_OUT_DIR -rf
 		exit 1
 		;;
-
+		
 		*)
-		BUILD_KERNEL_OUT_DIR=$BUILD_TOP_DIR/okernel/$BUILD_COMMAND
-		PRODUCT_OUT=$BUILD_TOP_DIR/okernel/$BUILD_COMMAND
-
+		BUILD_KERNEL_OUT_DIR=$BUILD_TOP_DIR/okernel
+		PRODUCT_OUT=$BUILD_TOP_DIR/okernel
+		
 		BOARD_KERNEL_BASE=0x00000000
 		BOARD_KERNEL_PAGESIZE=2048
 		BOARD_KERNEL_TAGS_OFFSET=0x01E00000
 		BOARD_RAMDISK_OFFSET=0x02000000
+		BOARD_KERNEL_CMDLINE="console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3"
 		#BOARD_KERNEL_CMDLINE="console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3"
-		#BOARD_KERNEL_CMDLINE="console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3"
-		BOARD_KERNEL_CMDLINE="console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x37 ehci-hcd.park=3 androidboot.selinux=permissive"
 		mkdir -p $BUILD_KERNEL_OUT_DIR
 		;;
 
@@ -169,7 +130,7 @@ FUNC_APPEND_DTB()
 }
 
 INSTALLED_DTIMAGE_TARGET=${BUILD_KERNEL_OUT_DIR}/dt.img
-DTBTOOL=$BUILD_KERNEL_DIR/release-tools/dtbToolCM
+DTBTOOL=$BUILD_TOP_DIR/kernel/tools/dtbTool
 
 FUNC_BUILD_DTIMAGE_TARGET()
 {
@@ -180,6 +141,13 @@ FUNC_BUILD_DTIMAGE_TARGET()
 	echo ""
 	echo "DT image target : $INSTALLED_DTIMAGE_TARGET"
 	
+	if ! [ -e $DTBTOOL ] ; then
+		if ! [ -d $BUILD_TOP_DIR/out/host/linux-x86/bin ] ; then
+			mkdir -p $BUILD_TOP_DIR/out/host/linux-x86/bin
+		fi
+		cp $BUILD_TOP_DIR/kernel/tools/dtbTool $DTBTOOL
+	fi
+
 	echo "$DTBTOOL -o $INSTALLED_DTIMAGE_TARGET -s $BOARD_KERNEL_PAGESIZE \
 		-p $BUILD_KERNEL_OUT_DIR/scripts/dtc/ $BUILD_KERNEL_OUT_DIR/arch/arm/boot/"
 		$DTBTOOL -o $INSTALLED_DTIMAGE_TARGET -s $BOARD_KERNEL_PAGESIZE \
@@ -207,18 +175,16 @@ FUNC_BUILD_KERNEL()
 	echo "build secure option="$SECURE_OPTION ""
 	echo "build SEANDROID option="$SEANDROID_OPTION ""
 
-	if [ "$BUILD_COMMAND" == "" ]; then
-		SECFUNC_PRINT_HELP;
-		exit -1;
-	fi
-
-	if ! [ -e $PRODUCT_OUT/ramdisk.img ] ; then
-		FUNC_MKRAMDISKIMG
-	fi
+        if [ "$BUILD_COMMAND" == "" ]; then
+                SECFUNC_PRINT_HELP;
+                exit -1;
+        fi
 
 	make -C $BUILD_KERNEL_DIR O=$BUILD_KERNEL_OUT_DIR -j$BUILD_JOB_NUMBER ARCH=arm \
 			CROSS_COMPILE=$BUILD_CROSS_COMPILE \
-			$KERNEL_DEFCONFIG VARIANT_DEFCONFIG=$VARIANT_DEFCONFIG SELINUX_DEFCONFIG=$SELINUX_DEFCONFIG || exit -1
+			$KERNEL_DEFCONFIG VARIANT_DEFCONFIG=$VARIANT_DEFCONFIG \
+			DEBUG_DEFCONFIG=$DEBUG_DEFCONFIG SELINUX_DEFCONFIG=$SELINUX_DEFCONFIG \
+			SELINUX_LOG_DEFCONFIG=$SELINUX_LOG_DEFCONFIG || exit -1
 
 	make -C $BUILD_KERNEL_DIR O=$BUILD_KERNEL_OUT_DIR -j$BUILD_JOB_NUMBER ARCH=arm \
 			CROSS_COMPILE=$BUILD_CROSS_COMPILE || exit -1
@@ -233,27 +199,6 @@ FUNC_BUILD_KERNEL()
 	echo ""
 }
 
-RAMDISK_SRC_DIR=$BUILD_TOP_DIR/sc04f_boot_ramdisk
-RAMDISK_TMP_DIR=/tmp/sc04f_boot_ramdisk
-FUNC_MKRAMDISKIMG()
-{
-    echo copy $RAMDISK_SRC_DIR to $(dirname $RAMDISK_TMP_DIR)
-
-    if [ -d $RAMDISK_TMP_DIR ]; then
-        rm -rf $RAMDISK_TMP_DIR
-    fi
-    cp -a $RAMDISK_SRC_DIR $(dirname $RAMDISK_TMP_DIR)
-    rm -rf $RAMDISK_TMP_DIR/.git
-    find $RAMDISK_TMP_DIR -name .gitkeep | xargs rm --force
-    find $RAMDISK_TMP_DIR -name .gitignore | xargs rm --force
-    if [ -f $RAMDISK_TMP_DIR/recovery_version ]; then
-        rm -f $RAMDISK_TMP_DIR/recovery_version
-    fi
-
-    $BUILD_KERNEL_DIR/release-tools/mkbootfs ${RAMDISK_TMP_DIR} > $PRODUCT_OUT/ramdisk.cpio
-    $BUILD_KERNEL_DIR/release-tools/minigzip < $PRODUCT_OUT/ramdisk.cpio > $PRODUCT_OUT/ramdisk.img
-}
-
 FUNC_MKBOOTIMG()
 {
 	echo ""
@@ -261,7 +206,14 @@ FUNC_MKBOOTIMG()
 	echo "START : FUNC_MKBOOTIMG"
 	echo "==================================="
 	echo ""
-	MKBOOTIMGTOOL=$BUILD_KERNEL_DIR/release-tools/mkbootimg
+	MKBOOTIMGTOOL=$BUILD_TOP_DIR/kernel/tools/mkbootimg
+
+	if ! [ -e $MKBOOTIMGTOOL ] ; then
+		if ! [ -d $BUILD_TOP_DIR/out/host/linux-x86/bin ] ; then
+			mkdir -p $BUILD_TOP_DIR/out/host/linux-x86/bin
+		fi
+		cp $BUILD_TOP_DIR/kernel/tools/mkbootimg $MKBOOTIMGTOOL
+	fi
 
 	echo "Making boot.img ..."
 	echo "	$MKBOOTIMGTOOL --kernel $KERNEL_ZIMG \
@@ -293,20 +245,20 @@ FUNC_MKBOOTIMG()
 	fi
 
 	cd $PRODUCT_OUT
-    tar cvf boot_${MODEL}_${CARRIER}_${CERTIFICATION}.tar boot.img
+	tar cvf boot_${MODEL}_${CARRIER}_${CERTIFICATION}.tar boot.img
 
-	if ! [ -d $BUILD_TOP_DIR/kernel/okernel/$BUILD_COMMAND ] ; then
-		mkdir -p $BUILD_TOP_DIR/kernel/okernel/$BUILD_COMMAND
+	if ! [ -d $BUILD_TOP_DIR/kernel/okernel ] ; then
+		mkdir -p $BUILD_TOP_DIR/kernel/okernel
 	else
-		rm $BUILD_TOP_DIR/kernel/okernel/$BUILD_COMMAND/boot_${MODEL}_${CARRIER}_${CERTIFICATION}.tar -f
+		rm $BUILD_TOP_DIR/kernel/okernel/boot_${MODEL}_${CARRIER}_${CERTIFICATION}.tar -f
 	fi
 
-#        echo ""
-#	echo "================================================="
-#        echo "-->Note, copy to $BUILD_TOP_DIR/../output/ directory"
-#	echo "================================================="
-#	cp ../$PRODUCT_OUT/boot_${MODEL}_${CARRIER}_${CERTIFICATION}.tar $BUILD_TOP_DIR/../../output/boot_${MODEL}_${CARRIER}_${CERTIFICATION}.tar || exit -1
-#        cd ~
+        echo ""
+	echo "================================================="
+        echo "-->Note, copy to $BUILD_TOP_DIR/../output/ directory"
+	echo "================================================="
+	cp $PRODUCT_OUT/boot_${MODEL}_${CARRIER}_${CERTIFICATION}.tar $BUILD_TOP_DIR/../output/boot_${MODEL}_${CARRIER}_${CERTIFICATION}.tar || exit -1
+        cd ~
 
 	echo ""
 	echo "==================================="
@@ -345,11 +297,6 @@ SECFUNC_PRINT_HELP()
 	echo "      klte_tmo"
 	echo "      klte_vzw"
 	echo "      klte_usc"
-	echo "      klte_dcm"
-	echo "      kactivelte_att"
-	echo "      ksportslte_spr"
-	echo "      slte_att"
-	echo "      pateklte_ctc"
 	echo "  \$2 : "
 	echo "      -B or Nothing  (-B : Secure Binary)"
 	echo "  \$3 : "
